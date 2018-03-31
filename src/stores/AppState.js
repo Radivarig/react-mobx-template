ca68d963e84776875f69d700a70fba271b179e0c
Radivarig <reslav.hollos@gmail.com>
2018-03-31 11:34:10 +0200

    Remove unsused field

--- src/stores/AppState.js
+++ src/stores/AppState.js
@@ -2,11 +2,9 @@ import { observable } from "mobx"
 
 export default class AppState {
   @observable timer: number
-  @observable issues: Array<Object>
 
   constructor () {
     this.timer = 0
-    this.issues = []
 
     setInterval (() => {
       this.timer += 1
